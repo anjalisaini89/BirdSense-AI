db3f02ef9e7def6d1c06db46c67dc03b5fdab59a
Anjali Saini <anjalisaini8996@gmail.com>
2026-08-05 22:19:09 +0530

    Update extract_features.py

--- backend/extract_features.py
+++ backend/extract_features.py
@@ -6,3 +6,23 @@ from tqdm import tqdm
 
 from config import DATASET_DIR, FEATURES_DIR, SAMPLE_RATE, DURATION, N_MFCC
 from utils import get_audio_files
+
+def load_audio(file_path):
+
+    audio, sr = librosa.load(
+        file_path,
+        sr=SAMPLE_RATE
+    )
+
+    required_length = SAMPLE_RATE * DURATION
+
+    if len(audio) > required_length:
+        audio = audio[:required_length]
+
+    else:
+        audio = np.pad(
+            audio,
+            (0, required_length - len(audio))
+        )
+
+    return audio

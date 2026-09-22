33550ee5f18dd8bf8611ed1a52e8bb61209dadad
Anjali Saini <anjalisaini8996@gmail.com>
2026-09-22 17:40:49 +0530

    Create birdInfo.js

---
+++ frontend/src/birdInfo.js
@@ -0,0 +1,21 @@
+const birdInfo = {
+  "Bartletts Tinamou": {
+    habitat: "Tropical forests",
+    region: "Peru, Ecuador",
+    conservation: "Least Concern"
+  },
+
+  "Blue-throated Piping Guan": {
+    habitat: "Humid forests",
+    region: "Brazil",
+    conservation: "Vulnerable"
+  },
+
+  "Collared Brushturkey": {
+    habitat: "Rainforests",
+    region: "Australia",
+    conservation: "Least Concern"
+  }
+};
+
+export default birdInfo;
